7194a82469228b9645d9f628579eb0ca61a55b5a
leoReiis <leonardoreis.ufsj@gmail.com>
2023-08-11 23:45:59 -0300

    feat: novo componente Menu

--- banco-talentos/src/App.vue
+++ banco-talentos/src/App.vue
@@ -1,67 +1,23 @@
 <template>
-  <div class="page-container">
-
-    <div class="nav-bar">
-
-      <router-link to="/" class="nav-item">
-        <div class="nav-item">
-          <a href="">Home</a>
-          <img src="/home.png" alt="Home" height="30" width="30">
-        </div>
-      </router-link>
-
-      <router-link to="/talentos" class="nav-item">
-        <div class="nav-item">
-          <a href="">Talentos</a>
-          <img src="/talento.png" alt="Home" height="30" width="30">
-        </div>
-      </router-link>
-
-      <router-link to="/sobre" class="nav-item">
-        <div class="nav-item">
-          <a href="">Sobre</a>
-          <img src="/sobre.png" alt="Home" height="30" width="30">
-        </div>
-      </router-link>
-
-    </div>
-
-    <br><br><br><br>
-    <About></About>
-    <Home></Home>
-    <Talentos></Talentos>
+  <div class="main-container">
+    <Menu />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import About from './views/about/About.vue'
-import Home from './views/Home/Home.vue'
-import Talentos from './views/Talentos/Talentos.vue'
+import Menu from './components/Menu/Menu.vue'
 
 export default {
-  components: { About, Home, Talentos }
+  components: { Menu }
 }
 </script>
 
 
 <style>
-.page-container {
+.main-container {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-.nav-bar {
-  display: flex;
-  gap: 50px;
-}
-
-.nav-item {
-  border: solid 2px #9EADBA;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  width: 150px;
-  height: 40px;
 }
 </style>
---
+++ banco-talentos/src/components/Menu/Menu.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="page-container">
+
+    <div class="nav-bar">
+
+      <router-link to="/" class="nav-item">
+        <div class="nav-item">
+          <a href="">Home</a>
+          <img src="/home.png" alt="Home" height="30" width="30">
+        </div>
+      </router-link>
+
+      <router-link to="/talentos" class="nav-item">
+        <div class="nav-item">
+          <a href="">Talentos</a>
+          <img src="/talento.png" alt="Home" height="30" width="30">
+        </div>
+      </router-link>
+
+      <router-link to="/sobre" class="nav-item">
+        <div class="nav-item">
+          <a href="">Sobre</a>
+          <img src="/sobre.png" alt="Home" height="30" width="30">
+        </div>
+      </router-link>
+
+    </div>
+
+    <br><br><br><br>
+
+  </div>
+</template>
+
+<script>
+import About from '../../views/About/About.vue'
+import Home from '../../views/Home/Home.vue'
+import Talentos from '../../views/Talentos/Talentos.vue'
+
+
+export default {
+  components: { About, Home, Talentos }
+}
+
+</script>
+
+
+<style>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.nav-bar {
+  display: flex;
+  gap: 50px;
+}
+
+.nav-item {
+  border: solid 2px #9EADBA;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 150px;
+  height: 40px;
+}
+</style>
